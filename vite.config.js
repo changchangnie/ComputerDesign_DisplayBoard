9332e836d03e28e9f321df087bef4386fd563c07
doudouchen <3179369731@qq.com>
2025-04-12 17:59:39 +0800

    Vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
+
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    server: {
-        open: true
+    plugins: [
+        vue(),
+    ],
+    base: './',
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-}) 
+})
