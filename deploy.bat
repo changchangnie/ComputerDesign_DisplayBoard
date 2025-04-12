a182a3e812b745272eeae6fd5a0f75777754fc9a
doudouchen <3179369731@qq.com>
2025-04-12 19:29:40 +0800

    1

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,7 @@
 @echo off
 echo Starting to deploy to GitHub Pages...
 
+rem 添加变更
 git add .
 git commit -m "1"
 git push -u origin master
@@ -11,13 +12,13 @@ call npm run build
 rem 进入构建目录
 cd dist
 
-rem 初始化git仓库
+rem 初始化 git 仓库
 git init
 git add -A
 git commit -m "deploy"
 
-rem 推送到gh-pages分支
-git push -f git@github.com:changchangnie/changchangnie.github.io.git master
+rem 推送到 GitHub 用户的 gh-pages 分支
+git push -f git@github.com:changchangnie/changchangnie.github.io.git master:gh-pages
 
 rem 返回原目录
 cd ..
